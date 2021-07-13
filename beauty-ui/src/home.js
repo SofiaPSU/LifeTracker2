fd5ad0fe0abd61ac98a34960793af89d88c16608
Kelsey <khetherington@berkeley.edu>
2021-07-13 11:23:36 -0700

    creating components folder

--- beauty-ui/src/home.js
+++
@@ -1,9 +0,0 @@
-import './home.css';
-import Navbar from "../Navbar/Navbar"
-
-homeBody() {
-    //include texts and images from first screen 
-    <div>rectangle1</div>
-    <div>image1</div>
-    <div>image2</div>
-}
