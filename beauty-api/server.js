5d435874bc27159829a8e153cbb70874a123e89e
SofiaPSU <sxr5616@psu.edu>
2021-07-14 17:15:43 -0400

    Added token and started working on ui for registration

--- beauty-api/server.js
+++ beauty-api/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
 const { PORT } = require("./config")
+const authRoutes = require("./routes/auth")
 
 //added by Kelsey
 // const homePage = require("./routes/home")
