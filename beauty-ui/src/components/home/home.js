08ff00c5441ceace62f408cf183c8f10233cda5b
Kelsey <khetherington@berkeley.edu>
2021-07-15 15:55:17 -0700

    end of Thursday

--- beauty-ui/src/components/home/home.js
+++ beauty-ui/src/components/home/home.js
@@ -3,9 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from "@material-ui/core/Button"
 import { Box, Typography, makeStyles } from "@material-ui/core"
+import Container from '@material-ui/core/Container';
 
 
-var backgroundimg = <img src="https://images.unsplash.com/photo-1598412795976-9c195182ee01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2134&q=80" alt="background"/>
 var moisturizers= <img src="https://images.unsplash.com/photo-1575410229391-19b4da01cc94?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fGZhY2UlMjBtb2lzdHVyaXplcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="moisturizers"/>
 var cleansers= <img src="https://unsplash.com/photos/X1sIr53DhzA" alt="cleansers"/>
 var serums= <img src="https://images.unsplash.com/photo-1600180583258-6d9b0c7b782b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHNlcnVtc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" alt="serums"/>
@@ -14,14 +14,16 @@ var mascaras= <img src="https://images.unsplash.com/photo-1560725613-4b52e67fc67
 var foundation= <img src="https://images.unsplash.com/photo-1607602132700-068258431c6c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=564&q=80" alt="foundation"/>
 var perfume= <img src="https://images.unsplash.com/photo-1622618991746-fe6004db3a47?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fHBlcmZ1bWV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="perfume"/>
 
+
 //useStyles is like CSS in js
 const useStyles = makeStyles((theme) => ({
 wrapper: {
     height: '100vh',
+    width: '100vw',
     background: `url(https://images.unsplash.com/photo-1598412795976-9c195182ee01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2134&q=80)`,
     backgroundRepeat: 'no-repeat',
-    backgroundSize: 'contain',
-    },
+    backgroundSize: 'cover',
+  },
   title: {
     fontFamily: "Euclid Circular A",
     fontStyle: "normal",
@@ -38,8 +40,9 @@ wrapper: {
   registerBTN: {
     marginTop: 10, // space between outer edge and adjacent elements 
     padding: 20, //space between content , outer edge
-    width: 200
-  },
+    width: 200,
+    p: "#8F5338",
+  }
 }));
 
 export default function Home() {
@@ -51,6 +54,7 @@ export default function Home() {
   };
 
   return (
+
       <Box className={classes.wrapper}>
           <Typography variant="h1" className={classes.title}>
               Making Beauty Sustainable
@@ -58,9 +62,10 @@ export default function Home() {
           <Typography variant="body1" className={classes.subtitle}>
           Donate or Recycle Your Makeup Products, learn More About SustainabilityThe beauty industry creates 120 billion units of packaging every year. In 2015, research found that packaging accounted for 146 million tonnes of plastic every year.
           </Typography>
-          <Button variant="contained" color="primary" className={classes.registerBTN}>
+          <Button variant="contained" color="default" className={classes.registerBTN}>
               Register
           </Button>
       </Box>
   );
 }
+
