799d11281fd33cfbecb09de177a599743db528b1
Kelsey <khetherington@berkeley.edu>
2021-07-21 13:53:33 -0700

    kelsey changes

--- beauty-ui/src/components/give/giveSuccess.js
+++ beauty-ui/src/components/give/giveSuccess.js
@@ -1,12 +1,68 @@
+import './giveSuccess.css';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
+const mapStyles = {
+  width: 400,
+  height: '100%'
+};
 
-export default function GiveSucess(){
+// export default function GiveSucess(){
 
-return(
-    <div className="giveSuccess">
-      Thank you for your Submission!
-    </div>
-);
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,  // Hides or shows the InfoWindow
+    activeMarker: {},          // Shows the active marker upon click
+    selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+  };
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
 
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInnpmfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
 
+  render() {
+    return (
+
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: 37.7749,
+            lng: 122.4194
+          }
+        }
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Kenyatta International Convention Centre'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
 }
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAYlQ6lsXJey1Uaca8vUVExDcHP4TLGgis'
+})(MapContainer);
