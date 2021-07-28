9f7ad94fcc2bf9390464553fa19557b6782cb836
Kelsey <khetherington@berkeley.edu>
2021-07-28 08:38:44 -0700

    kelsey work

--- beauty-api/models/newp.js
+++ beauty-api/models/newp.js
@@ -1,12 +1,12 @@
 const db = require("../db")
 const {BadRequestError} = require("../utils/errors")
 
-class Settings{
+class newp{
     static async updateInfo({ user, settings }){
         if(!user){
             throw new BadRequestError("No authentication recognized")
         }
-        const requiredFields = ["zip_code","first_name","last_name", "email", "age"]
+        const requiredFields = ["password"]
         requiredFields.forEach(field =>{
             if(!settings.hasOwnProperty(field)){
                 throw new BadRequestError(`Missing ${field} in request body`)
@@ -14,17 +14,13 @@ class Settings{
         })
         const query = `
         UPDATE users SET
-            zip_code = $2,
-            first_name = $3,
-            last_name = $4,
-            email = $5,
-            age = $6
+            password = $2
         WHERE username= $1;`
-        const result = await db.query(query, [ user.username, settings.zip_code, settings.first_name, settings.last_name, settings.email, settings.age])
+        const result = await db.query(query, [ user.username, settings.password])
         const profile = result.rows[0]
         return profile
     }
 
 }
 
-module.exports = Settings
+module.exports = newp
--- beauty-ui/src/components/give/giveSuccess.js
+++ beauty-ui/src/components/give/giveSuccess.js
@@ -7,8 +7,20 @@ import Geocode from "react-geocode";
 const mapStyles = {
   width: '100%',
   height: "40%",
-  zoom: { lat: 40.7640954, lng: -122.2419132 },
 };
+
+const centers = [
+  { lat: 37.7749, lng: -122.4194},
+  { lat: 37.7993, lng:  -122.3977},
+  { lat: 37.8715, lng: -122.2730},
+  { lat: 37.7640954, lng: -122.2419132},
+  { lat: 37.8128, lng: -122.2610},
+];
+
+//use the haversine method to find nearest center to location 
+// private function nearestCenter(centers, location) {
+  
+// }
 
 // export default function GiveSuccess(){
 
@@ -66,37 +78,37 @@ export class MapContainer extends Component {
       <div className="googleMap">
       <Map
         google={this.props.google}
-        zoom={12}
+        zoom={11}
+        center={ { lat: this.state.userPosition.latitude, lng: this.state.userPosition.longitude } }
         style={mapStyles}
       >
-        <Marker
+        <Marker className="current location"
           position={ { lat: this.state.userPosition.latitude, lng: this.state.userPosition.longitude } }
           onClick={this.onMarkerClick}
-          name={'Hīrā Drop Off Center (1765 California St, San Francisco, CA 94109)'}
+          name={'Current Location'}
           
         />
-        <Marker
+        <Marker className="SF1"
           position={ {lat: 37.7749, lng: -122.4194} }
           onClick={this.onMarkerClick}
           name={'Hīrā Drop Off Center (1765 California St, San Francisco, CA 94109)'}
-          
         />
-        <Marker
+        <Marker className="SF2" 
           position={ {lat: 37.7993, lng: -122.3977} }
           onClick={this.onMarkerClick}
           name={'Hīrā Drop Off Center (1098 The Embarcadero, San Francisco, CA 94111)'}
         />
-        <Marker
+        <Marker className="Berkeley" 
           position={ {lat: 37.8715, lng: -122.2730} }
           onClick={this.onMarkerClick}
           name={'Hīrā Drop Off Center (2495 Bancroft Way, Berkeley, CA 94704)'}
         />
-        <Marker
+        <Marker className="Alameda" 
           position={ {lat: 37.7640954, lng: -122.2419132} }
           onClick={this.onMarkerClick}
           name={'Hīrā Drop Off Center (2201 Shore Line Dr, Alameda, CA 94501)'}
         />
-        <Marker
+        <Marker className="Oakland" 
           position={ {lat: 37.8128, lng: -122.2610} }
           onClick={this.onMarkerClick}
           name={'Hīrā Drop Off Center (230 Bay Pl, Oakland, CA 94612)'}
@@ -110,32 +122,13 @@ export class MapContainer extends Component {
             <h4>{this.state.selectedPlace.name}</h4>
           </div>
         </InfoWindow>
-        
-  
-        {/* <CurrentLocation
-        centerAroundCurrentLocation
-        google={this.props.google}
-        >
-         */}
-      {/* <Marker 
-        onClick={this.onMarkerClick} 
-        name={'Current Location'} />
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
-          onClose={this.onClose}
-        >
-          <div>
-            <h4>{this.state.selectedPlace.name}</h4>
-          </div>
-        </InfoWindow> */}
-      {/* </CurrentLocation> */}
       </Map>
       </div>
       </div>
     );
   }
 }
+
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyAYlQ6lsXJey1Uaca8vUVExDcHP4TLGgis'
